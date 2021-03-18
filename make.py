410f9fd2a1f702ce2bdc6302a508f04edb204cb3
Marek Czerski <m.czerski@ap-tech.pl>
2021-03-18 17:04:37 +0100

    add opensbi to buildroo, enlarge l2 cache

--- make.py
+++ make.py
@@ -49,7 +49,7 @@ def main():
     soc_kwargs = {
         "integrated_sram_size": 0x0,
         "integrated_rom_size": 0x400,
-        "l2_size" : 0x2000, # Use Wishbone and L2 for memory accesses.
+        "l2_size" : 0x4000, # Use Wishbone and L2 for memory accesses.
         "uart_baudrate": 1e6,
         "uart_fifo_depth": 1024,
     }
